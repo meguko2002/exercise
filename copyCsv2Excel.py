8d68ce464931e8694040508abc1ae498d6785105
poko-PC\poko <meguko2002@gmail.com>
2019-01-14 22:55:11 +0900

    load excel file and save ~Page315

--- copyCsv2Excel.py
+++ copyCsv2Excel.py
@@ -2,28 +2,36 @@
 #removeCsvHeader.py
 
 import csv, os
+import openpyxl
 
-os.makedirs('headerRemoved', exist_ok=True)
+#入力ファイル、出力ファイルの保管フォルダを作成する
+os.makedirs('inputFiles', exist_ok=True)
+os.makedirs('outputFiles', exist_ok=True)
 
-#カレントディレクトリの全ファイルをループする
-for csv_filename in os.listdir('fileEdit'):
+#inputFilesフォルダの全ファイルをループする
+for csv_filename in os.listdir('inputFiles'):
     if not csv_filename.endswith('.csv'):
         continue #CSVファイルでなければスキップ
     print('見出し削除中' + csv_filename + '...')
 
-    # CSVファイルを読み込む（最初の行をスキップする）
+    # CSVファイルを読み込んで各行をcsv_rows[]に書き込む
     csv_rows = []
     csv_file_obj = open('fileEdit/' + csv_filename)
     reader_obj = csv.reader(csv_file_obj)
     for row in reader_obj:
-        if reader_obj.line_num == 1:
-            continue  #最初の行をスキップする
         csv_rows.append(row)
     csv_file_obj.close()
 
-    # CSVファイルを書き出す
-    csv_file_obj = open(os.path.join('headerRemoved',csv_filename), 'w', newline='')
-    csv_writer = csv.writer(csv_file_obj)
-    for row in csv_rows:
-        csv_writer.writerow(row)
-    csv_file_obj.close()
+    # 保存先のExceファイルを作成
+    wb = openpyxl.load_workbook('ooutputFile/' + csv_filename)
+    sheet = wb.get_sheet_by_name('Sheet')
+
+    # CSVファイルから読み込んだcsv_rows[]をExcelファイルに書き出す
+    for row_num in range(len(csv_rows)):  # 先頭行をスキップ
+        produce_name = sheet.cell(row=row_num, column=1).value
+        if produce_name in PRICE_UPDATES:
+            sheet.cell(row=row_num, column=2).value = PRICE_UPDATES[produce_name]
+
+    wb.save('fileEdit/updateProduceSales.xlsx')
+
+
